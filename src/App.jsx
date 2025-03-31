9269de9e1c994a7c3d8650e431e8d21ca2958182
John Sims <johnsims.dev@gmail.com>
2025-03-30 21:30:53 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Pizza from "./Pizza";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Padre Gino's Pizza – Order Now</h1>
+      <Pizza name="Pepperoni" description="Mozzarella Cheese, Pepperoni" />
+      <Pizza
+        name="The Hawaiian Pizza"
+        description="Sliced Ham, Pineapple, Mozzarella Cheese"
+      />
+      <Pizza
+        name="The Big Meat Pizza"
+        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+      />
+    </div>
+  );
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
