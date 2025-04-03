fe6fb9f718d9bd57a7a0694555f100afe3391d98
John Sims <johnsims.dev@gmail.com>
2025-04-02 22:21:44 -0400

    add JSON parser to express

--- backend/server/server.js
+++ backend/server/server.js
@@ -3,7 +3,13 @@ import cors from "cors";
 import { getAllUsers } from "./controllers/userController.js";
 
 const app = express();
+
+// Enable CORS
 app.use(cors());
+
+// Parse JSON bodies for all incoming requests
+app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/users", getAllUsers);
